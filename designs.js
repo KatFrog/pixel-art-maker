5929c986748d828ed31b2253c33b6ba9a6a1cdb4
Kathryn A Patterson <katp@wegrok.net>
2018-01-23 15:30:53 -0500

    Cleaning up the code

--- designs.js
+++ designs.js
@@ -3,39 +3,34 @@ let width;
 let height;
 let row_id_assign = "";
 let row_id = "";
-let cell_id_assign = "";
-let cell_id = "";
 
 // Variables for color sizePicker
 let grid_color;
 
-
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
-    let k;
     for (let j = 1; j <= height; ++j) {
-        // insert row tags
         row_id_assign = "<tr id=\"row" + j + "\">";
         row_id = "#row" + j;
         $('#pixel_canvas').append(row_id_assign);
         for (k = 1; k <= width; ++k) {
             const row = $(row_id).append("<td>");
             const cell = $(row).children(':last');
-            cell.on("click", function(evt){
+            // Set up event listeners for each cell using JavaScript closures
+            cell.on("click", function(evt) {
                 cell.attr("style", "background-color: " + grid_color);
             });
         };
-        //cell_id = "#cell" + k;
-        //$(cell_id).append("</tr>");
     };
 
 };
 
+// Clear the HTML table to allow a new grid to be created
 function clearGrid() {
     $('#pixel_canvas').empty();
 };
 
+// Wait until the DOM is loaded before running jQuery code
 document.addEventListener('DOMContentLoaded', function() {
     $('#size_button').on("click", function(evt) {
         evt.preventDefault();
