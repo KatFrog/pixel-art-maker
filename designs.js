2fe92aecdb992af4c7cdbe77e098f1c509905779
Kathryn A Patterson <katp@wegrok.net>
2018-01-23 15:25:53 -0500

    Added function to clear the grid, got pixel canvas working

--- designs.js
+++ designs.js
@@ -1,4 +1,3 @@
-// include jQueary
 // Variables for drawing grid
 let width;
 let height;
@@ -19,23 +18,30 @@ function makeGrid() {
         // insert row tags
         row_id_assign = "<tr id=\"row" + j + "\">";
         row_id = "#row" + j;
-        console.log(row_id);
         $('#pixel_canvas').append(row_id_assign);
         for (k = 1; k <= width; ++k) {
-            cell_id_assign = "<td id=\"cell" + k + "\"></td>";
-            $(row_id).append(cell_id_assign);
+            const row = $(row_id).append("<td>");
+            const cell = $(row).children(':last');
+            cell.on("click", function(evt){
+                cell.attr("style", "background-color: " + grid_color);
+            });
         };
-        cell_id = "#cell" + k;
-        $(cell_id).append("</tr>");
-    }
+        //cell_id = "#cell" + k;
+        //$(cell_id).append("</tr>");
+    };
 
-}
+};
+
+function clearGrid() {
+    $('#pixel_canvas').empty();
+};
 
 document.addEventListener('DOMContentLoaded', function() {
     $('#size_button').on("click", function(evt) {
         evt.preventDefault();
         width = $('#input_width').val();
         height = $('#input_height').val();
+        clearGrid();
         makeGrid();
     });
     $('#colorPicker').on('change', function(evt) {
