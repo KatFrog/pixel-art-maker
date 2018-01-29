8911010b70bea35ed1907c29b57533c644fcb3f7
Kathryn A Patterson <katp@wegrok.net>
2018-01-28 22:00:18 -0500

    Cleaned up code

--- designs.js
+++ designs.js
@@ -14,8 +14,7 @@ function makeGrid() {
         row_id = "#row" + j;
         $('#pixel_canvas').append(row_id_assign);
         for (let k = 1; k <= width; ++k) {
-            const row = $(row_id).append("<td>");
-            const cell = $(row).children(':last');
+            $(row_id).append("<td>");
         };
     };
 };
@@ -26,19 +25,33 @@ function clearGrid() {
 };
 
 // Wait until the DOM is loaded before running jQuery code
-document.addEventListener('DOMContentLoaded', function() {
+$(document).ready(function() {
     $('#size_button').on("click", function(evt) {
         evt.preventDefault();
         width = $('#input_width').val();
         height = $('#input_height').val();
-        clearGrid();
-        makeGrid();
+        if ((width > 40) || (width <= 0)){
+            alert("Please pick a width between 1 and 40.");
+        } else if ((height > 40) || (height <= 0)){
+            alert("Please pick a height between 1 and 40.");
+        } else {
+            clearGrid();
+            makeGrid();
+        }
     });
     $('#colorPicker').on('change', function(evt) {
         grid_color = $(this).val();
     });
     $('#clear-canvas').on("click", function() {
-        $("#pixel_canvas").find("td").css("background-color", "rgb(254,253,230)")});
+        $("#pixel_canvas").find("td").css("background-color", "rgb(254,253,230)")
+    });
     $('#pixel_canvas').on('click', 'td', function(evt) {
-        $(evt.target).attr('style', 'background-color: ' + grid_color)});
+        if ($(evt.target).hasClass('clicked')) {
+            $(evt.target).attr('style', 'background-color: rgb(254,253,230)');
+            $(evt.target).removeClass('clicked');
+        } else {
+            $(evt.target).attr('style', 'background-color: ' + grid_color);
+            $(evt.target).addClass('clicked');
+        };
+    });
 });
