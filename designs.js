66f1a98ccb0939148da3a70294e3fb12a0ba2a77
Kathryn A Patterson <katp@wegrok.net>
2018-01-30 15:22:22 -0500

    Made a few const for background color and default height/weight

--- designs.js
+++ designs.js
@@ -1,15 +1,16 @@
-// Variables for drawing grid
-let width = 10;
-let height = 10;
-let row_id_assign = "";
-let row_id = "";
-let isDrawing = false;
-
-// Variables for color sizePicker
+// Variables and constants for the Pixel Art Maker
 let grid_color = 'rgb(0,0,0)';
+let isDrawing = false;
+const bgColor = "rgb(254,253,230)";
+const defaultHeight = 10;
+const defaultWidth = 10;
+
+
 
 // When size is submitted by the user, call makeGrid()
-function makeGrid() {
+function makeGrid(height, width) {
+    let row_id_assign = "";
+    let row_id = "";
     for (let j = 1; j <= height; ++j) {
         row_id_assign = "<tr id=\"row" + j + "\">";
         row_id = "#row" + j;
@@ -29,20 +30,20 @@ function clearGrid() {
 $(document).ready(function() {
 
     // Initial setup of the drawing grid
-    makeGrid();
+    makeGrid(defaultHeight, defaultWidth);
 
     // Event listener for setting up the grid
     $('#size_button').on("click", function(evt) {
         evt.preventDefault();
-        width = $('#input_width').val();
-        height = $('#input_height').val();
-        if ((width > 40) || (width <= 0)){
-            alert("Please pick a width between 1 and 40.");
-        } else if ((height > 40) || (height <= 0)){
-            alert("Please pick a height between 1 and 40.");
+        let width = $('#input_width').val();
+        let height = $('#input_height').val();
+        if ((width > 60) || (width <= 0)){
+            alert("Please pick a width between 1 and 60.");
+        } else if ((height > 60) || (height <= 0)){
+            alert("Please pick a height between 1 and 60.");
         } else {
             clearGrid();
-            makeGrid();
+            makeGrid(height, width);
         }
     });
 
@@ -53,13 +54,13 @@ $(document).ready(function() {
 
     // Event listener to clear the canvas
     $('#clear-canvas').on("click", function() {
-        $("#pixel_canvas").find("td").css("background-color", "rgb(254,253,230)")
+        $("#pixel_canvas").find("td").css("background-color", bgColor)
     });
 
     // Event listener to change the background color on a single click
     $('#pixel_canvas').on('click', 'td', function(evt) {
         if ($(evt.target).hasClass('clicked')) {
-            $(evt.target).attr('style', 'background-color: rgb(254,253,230)');
+            $(evt.target).attr('style', 'background-color: ' + bgColor);
             $(evt.target).removeClass('clicked');
         } else {
             $(evt.target).attr('style', 'background-color: ' + grid_color);
