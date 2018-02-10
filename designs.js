8f20b4b3a668187234625fd298fcff149d8c72da
Kathryn Patterson <katp@wegrok.net>
2018-02-10 15:09:38 -0500

    Fixed files to comply with Codacy

--- designs.js
+++ designs.js
@@ -1,5 +1,5 @@
 // Variables and constants for the Pixel Art Maker
-let grid_color = 'rgb(0,0,0)';
+let gridColor = "rgb(0,0,0)";
 let isDrawing = false;
 const bgColor = "rgb(254,253,230)";
 const defaultHeight = 10;
@@ -9,21 +9,21 @@ const defaultWidth = 10;
 
 // When size is submitted by the user, call makeGrid()
 function makeGrid(height, width) {
-    let row_id_assign = "";
-    let row_id = "";
+    let rowId_assign = "";
+    let rowId = "";
     for (let j = 1; j <= height; ++j) {
-        row_id_assign = "<tr id=\"row" + j + "\">";
-        row_id = "#row" + j;
-        $('#pixel_canvas').append(row_id_assign);
+        rowId_assign = "<tr id=\"row" + j + "\">";
+        rowId = "#row" + j;
+        $("#pixel_canvas").append(rowId_assign);
         for (let k = 1; k <= width; ++k) {
-            $(row_id).append("<td>");
-        };
-    };
-};
+            $(rowId).append("<td>");
+        }
+    }
+}
 
 // Clear the HTML table to allow a new grid to be created
 function clearGrid() {
-    $('#pixel_canvas').empty();
+    $("#pixel_canvas").empty();
 };
 
 // Wait until the DOM is loaded before running jQuery code
@@ -33,10 +33,10 @@ $(document).ready(function() {
     makeGrid(defaultHeight, defaultWidth);
 
     // Event listener for setting up the grid
-    $('#size-button').on("click", function(evt) {
+    $("#size-button").on("click", function(evt) {
         evt.preventDefault();
-        let width = $('#input_width').val();
-        let height = $('#input_height').val();
+        let width = $("#input_width").val();
+        let height = $("#input_height").val();
         if ((width > 60) || (width <= 0)){
             alert("Please pick a width between 1 and 60.");
         } else if ((height > 60) || (height <= 0)){
@@ -48,23 +48,23 @@ $(document).ready(function() {
     });
 
     // Event listener for color picker
-    $('#colorPicker').on('change', function(evt) {
-        grid_color = $(this).val();
+    $("#colorPicker").on("change", function(evt) {
+        gridColor = $(this).val();
     });
 
     // Event listener to clear the canvas
-    $('#clear-canvas').on("click", function() {
-        $("#pixel_canvas").find("td").css("background-color", bgColor)
+    $("#clear-canvas").on("click", function() {
+        $("#pixel_canvas").find("td").css("background-color", bgColor);
     });
 
     // Event listener to change the background color on a single click
-    $('#pixel_canvas').on('click', 'td', function(evt) {
-        if ($(evt.target).hasClass('clicked')) {
-            $(evt.target).attr('style', 'background-color: ' + bgColor);
-            $(evt.target).removeClass('clicked');
+    $("#pixel_canvas").on("click", "td", function(evt) {
+        if ($(evt.target).hasClass("clicked")) {
+            $(evt.target).attr("style", "background-color: " + bgColor);
+            $(evt.target).removeClass("clicked");
         } else {
-            $(evt.target).attr('style', 'background-color: ' + grid_color);
-            $(evt.target).addClass('clicked');
+            $(evt.target).attr("style", "background-color: " + gridColor);
+            $(evt.target).addClass("clicked");
         };
     });
 
@@ -80,8 +80,8 @@ $(document).ready(function() {
     // Event listener to paint the canvas continuously
     $("#pixel_canvas").mouseover(function(evt) {
         if (isDrawing) {
-            $(evt.target).attr('style', 'background-color: ' + grid_color);
-            $(evt.target).addClass('clicked');
+            $(evt.target).attr("style", "background-color: " + gridColor);
+            $(evt.target).addClass("clicked");
         }
     });
 });
